b0260ec51b49990361234c8fa973ce3782a54441
Ehab Charek <ehabcharek@hotmail.com>
2020-10-14 13:39:17 -0700

    added support for multiple files to the copy_to.py file

--- copy_to.py
+++ copy_to.py
@@ -1,9 +1,10 @@
 import easygui, shutil
 
-source = easygui.fileopenbox("Source File", default="d:\GIT\EMC Tools\emc_tools.py", filetypes= "*.py")
+source = easygui.fileopenbox("Source File", default="d:\GIT\EMC Tools\emc_tools.py", filetypes= "*.py", multiple=True)
 destination = easygui.diropenbox("Destination Folder", default=r"d:\MEGA\Blender Configs\2.80\scripts\addons")
 
-shutil.copy2(source, destination)
+for file in source:
+    shutil.copy2(file, destination)
 
 print(str(source))
 print(str(destination))
